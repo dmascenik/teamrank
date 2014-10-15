83308ee503776efec65b7fdb85b2cb96d95dda44
Dan Mascenik <dtm@umich.edu>
2014-10-14 20:56:25 -0700

    Fixed test coverage

---
+++ src/test/java/com/danmascenik/tools/teamrank/RankTest.java
@@ -0,0 +1,37 @@
+package com.danmascenik.tools.teamrank;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.danmascenik.tools.teamrank.Rank.RankComparator;
+import com.google.common.collect.Lists;
+
+public class RankTest {
+
+  @Test
+  public void testRankConstructor() {
+    Rank<String> rank = new Rank<String>("a", 1f);
+    assertEquals("a", rank.getId());
+    assertEquals(1f, rank.getScore(), 0f);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorWithNullId() {
+    new Rank<String>(null, 1f);
+  }
+
+  @Test
+  public void testComparator() {
+    @SuppressWarnings("unchecked")
+    List<Rank<String>> ranks = Lists.newArrayList(new Rank<String>("a", 0f), new Rank<String>("b", 0f),
+        new Rank<String>("c", -1f), new Rank<String>("d", 1f));
+    Collections.sort(ranks, new RankComparator());
+    assertEquals("d", ranks.get(0).getId());
+    assertEquals("c", ranks.get(3).getId());
+  }
+
+}
