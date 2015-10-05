0346ea54ae7b95c33d824f9feacc8837977830db
Dan Mascenik <dan@zoomdata.com>
2015-10-05 16:44:22 -0400

    Starting Flux implementation

--- client/test/ComboBoxTest.jsx
+++ client/test/ComboBoxTest.jsx
@@ -1,12 +1,19 @@
 var jsx = require('jsx-test').jsxTranspile(process.env.COVERAGE);
 var assert = require('assert');
+//var sinon = require('sinon');
+//var action = require('../src/ViewAction');
+
 
 describe('ComboBox', function() {
     var ComboBox = require('../src/ComboBox.jsx');
+    //var actionMock = sinon.mock(action);
+    //actionMock.expects("getSomething").once().returns("a mock");
+    //actionMock.expects("getSomething").never();
 
     describe('#render', function () {
         it('renders an input element', function () {
             jsx.assertRender(ComboBox, {}, '<input ');
+            //actionMock.verify();
         });
     });
 });
