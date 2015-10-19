d78f726064afa98b009b938ff124a433cc409bd1
Dan Mascenik <dan@zoomdata.com>
2015-10-18 22:12:18 -0400

    Getting started with material-ui

--- client/src/flux/component/TeamRankApp.jsx
+++ client/src/flux/component/TeamRankApp.jsx
@@ -1,10 +1,11 @@
 var React = require("react");
+var mui = require('material-ui');
 var Radium = require("radium");
 var Config = require('../store').ConfigStore;
 var ConfigAction = require('../action').ConfigActions;
 
 var style = {
-  base: {
+  title: {
     textAlign: 'left'
   }
 }
@@ -15,7 +16,8 @@ class TeamRankApp extends React.Component {
     this.state = {
       version: "unknown"
     }
-    this.onChange = this.onChange.bind(this)
+    this.onChange = this.onChange.bind(this);
+    this.showAbout = this.showAbout.bind(this);
   }
 
   componentDidMount() {
@@ -35,43 +37,51 @@ class TeamRankApp extends React.Component {
     }
   }
 
+  showAbout() {
+    this.refs.about.show();
+  }
+
+  hideAbout () {
+    this.refs.about.dismiss();
+  }
+
   render() {
-    return <div style={[style.base, this.props.style]}>
-      <span>
-      Version: {this.state.version}
-      <br/><br/>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet orci ullamcorper 
-      nunc consectetur eleifend. Sed dictum non tortor vel sagittis. Mauris venenatis congue nunc. 
-      Integer ac tellus vitae urna mattis iaculis vehicula vel risus. Pellentesque non faucibus velit, 
-      vel volutpat justo. Nulla sagittis molestie dolor, eget tincidunt metus ullamcorper finibus. 
-      Suspendisse imperdiet mollis rutrum.
-      <br/><br/>
-      Duis dignissim dui id risus egestas semper. Integer finibus ultrices risus quis malesuada. Donec 
-      vitae quam vulputate arcu cursus egestas vitae sit amet nibh. Pellentesque quis sapien dignissim, 
-      lobortis dui vitae, varius neque. Duis auctor purus sit amet sapien ultricies, ut semper nunc 
-      mattis. Nam mi nulla, dictum in nunc non, dignissim tristique lacus. In id tortor nisi. Aenean 
-      maximus dapibus sem. Nam molestie, metus id feugiat tempor, ante neque suscipit orci, vel 
-      venenatis odio enim eu magna. Nunc volutpat auctor lectus, eget pharetra ante ultricies nec. Nunc 
-      porta faucibus dolor at interdum. Mauris fermentum, est convallis finibus fermentum, mi orci 
-      faucibus lacus, vitae rutrum leo lectus nec velit. Phasellus eget pretium dolor. Integer cursus, 
-      orci at scelerisque mattis, tellus eros venenatis mauris, ut consectetur nulla quam et purus.
-      <br/><br/>
-      Nam at fermentum ipsum, in hendrerit ex. Nulla facilisi. Morbi a tincidunt lacus. Aenean volutpat 
-      aliquam metus vel vehicula. Maecenas lobortis tempus magna quis iaculis. Nulla facilisi. Vivamus 
-      risus ipsum, tristique in urna vel, semper tempor dui. Sed vitae metus volutpat mi rutrum ornare. 
-      Nunc venenatis quis mauris nec dignissim. Vestibulum ante ipsum primis in faucibus orci luctus et 
-      ultrices posuere cubilia Curae;
-      <br/><br/>
-      Sed libero arcu, viverra id tincidunt eget, auctor sit amet eros. Ut libero nibh, pretium sit 
-      amet gravida ac, ullamcorper at lacus. Sed id sollicitudin turpis. Maecenas aliquet metus et 
-      semper aliquet. Mauris et elit elit. Fusce ac ornare augue, facilisis malesuada est. Ut consequat, 
-      ipsum vel mattis vulputate, tellus elit placerat diam, quis lobortis sem tellus eu lacus. 
-      Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent 
-      sit amet urna egestas, ultricies felis eget, gravida mi. Nulla purus diam, maximus ac magna et, 
-      feugiat finibus sapien. Maecenas vitae euismod elit. Pellentesque rutrum nisi lacus, et ornare arcu 
-      mollis non. Etiam mollis massa at sapien cursus, at sagittis urna lobortis.
-      </span>
-    </div>
+
+    var customAction = <mui.RaisedButton
+                    label="OK"
+                    primary={true}
+                    onTouchTap={this.hideAbout} />;
+
+// <mui.CardMedia overlay={<mui.CardTitle title="Title" subtitle="Subtitle"/>}>
+//     <img src="http://lorempixel.com/600/337/nature/"/>
+//   </mui.CardMedia>
+
+
+    return <div><mui.AppBar title="TeamRank" 
+              style={style.title}
+              showMenuIconButton={false}
+              iconClassNameRight="fa fa-cogs"
+              onRightIconButtonTouchTap={this.showAbout}
+           /><center>
+              <mui.Card 
+                  style={{width: "50%"}}>
+                <mui.CardTitle title="TeamRank" subtitle="Makes ranking stuff better"/>
+                <mui.CardText>
+                  Lorem ipsum dolor sit amet, vim ne legimus consequat, duis harum 
+                  molestiae duo ut. In duo nihil latine incorrupte. Eum ut esse eius 
+                  constituto, admodum mentitum no his. Utroque epicurei expetenda eu 
+                  cum, at his quando volumus scriptorem, eos nisl inermis ei. In qui 
+                  brute intellegat, per te quando legere.
+                </mui.CardText>
+              </mui.Card>
+           </center>
+           <mui.Dialog
+              title="About TeamRank"
+                actions={customAction}
+                ref="about"
+                modal={false}>
+                        Version: {this.state.version}
+            </mui.Dialog></div>
   };
 
 }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,9 @@
 var React = require("react");
+var injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
 
 import TeamRankApp from './flux/component/TeamRankApp.jsx';
 //import ComboBox from './common/ComboBox.jsx';
 
 //React.render(<ComboBox width="200px"/>,              document.getElementById("combobox"));
-React.render(<TeamRankApp style={{width: "50%"}}/> ,  document.getElementById("app"));
+React.render(<TeamRankApp style={{width: "50%"}}/> ,  document.body);
