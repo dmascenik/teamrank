43dc8a0db5b2fe4840d73b2d938c39c4f92d88d3
Dan Mascenik <dan@zoomdata.com>
2015-10-26 14:58:04 -0400

    Supporting WebStorm IDE

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,7 +3,5 @@ var injectTapEventPlugin = require("react-tap-event-plugin");
 injectTapEventPlugin();
 
 import TeamRankApp from './flux/component/TeamRankApp.jsx';
-//import ComboBox from './common/ComboBox.jsx';
 
-//React.render(<ComboBox width="200px"/>,              document.getElementById("combobox"));
 React.render(<TeamRankApp style={{width: "50%"}}/> ,  document.body);
