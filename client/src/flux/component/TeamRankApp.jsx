7007a419a207116cd85ab9ebf21d9e85ef9ca11d
Dan Mascenik <dtm@umich.edu>
2015-10-29 18:29:11 -0400

    Fixed Header.jsx

--- client/src/flux/component/TeamRankApp.jsx
+++ client/src/flux/component/TeamRankApp.jsx
@@ -2,21 +2,14 @@ var React = require("react");
 
 // Components
 var ComboBox = require('../../common').ComboBox;
-var LoginInfo = require('../../common').LoginInfo;
 var mui = require('material-ui');
 var tr = require('./composite');
 
 // Flux parts
 var Config = require('../store').ConfigStore;
-var Login = require('../store').LoginStore;
 var ConfigAction = require('../action').ConfigActions;
-var LoginAction = require('../action').LoginActions;
 
 var Radium = require("radium");
-var style = {
-  title: { textAlign: 'left', position: 'fixed' },
-  login: { color: 'white', fontWeight: '600' }
-}
 
 /**
  * This is the outermost view-controller of the TeamRank web application.
@@ -25,8 +18,7 @@ class TeamRankApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      version: "unknown",
-      isLoggedIn: false
+      version: "unknown"
     }
     this.onChange = this.onChange.bind(this);
   }
@@ -36,21 +28,16 @@ class TeamRankApp extends React.Component {
    */
   componentDidMount() {
     Config.addChangeListener(this.onChange);
-    Login.addChangeListener(this.onChange);
     ConfigAction.loadConfig();
   }
 
   componentWillUnmount() {
     Config.removeChangeListener(this.onChange);
-    Login.removeChangeListener(this.onChange);
   }
 
   onChange() {
     if (Config.isConfigLoaded()) {
       this.setState({ version: Config.getConfig().version });
-    }
-    if (Login.isLoggedIn() != this.state.isLoggedIn) {
-      this.setState({ isLoggedIn: Login.isLoggedIn() });
     }
   }
 
@@ -60,23 +47,7 @@ class TeamRankApp extends React.Component {
       return <div />
     }
 
-    var extraProps = {};
-    if (Login.getDisplayName()) extraProps.displayName = Login.getDisplayName();
-    if (Login.getEmail()) extraProps.email = Login.getEmail();
-    if (Login.getAuthProvider()) extraProps.authProvider = Login.getAuthProvider();
-    if (Login.getAvatarUrl()) extraProps.avatarUrl = Login.getAvatarUrl();    
-
-    return <div><mui.AppBar title="TeamRank"
-              style={style.title}
-              showMenuIconButton={false}
-              iconElementRight={
-                <LoginInfo onSignIn={LoginAction.onSignIn} 
-                           onSignOut={LoginAction.onSignOut} 
-                           isSignedIn={this.state.isLoggedIn}
-                           style={style.login}
-                           {...extraProps} />
-              }
-              zDepth={1} />
+    return <div><tr.Header />
           <center>
               <mui.Card style={{width: "50%", paddingTop: "70px"}}>
                 <mui.CardTitle title="TeamRank" subtitle="Makes ranking stuff better"/>
--- client/src/flux/component/composite/Header.jsx
+++ client/src/flux/component/composite/Header.jsx
@@ -1,25 +1,61 @@
 var React = require("react");
-var Radium = require("radium");
 
-var Login = require('../../store').LoginStore;
-
+// Components
 var mui = require('material-ui');
+var LoginInfo = require('../../../common').LoginInfo;
 
+// Flux parts
+var LoginStore = require('../../store').LoginStore;
+var LoginAction = require('../../action').LoginActions;
+
+// Style management
+var Radium = require("radium");
+var style = {
+  title: { textAlign: 'left', position: 'fixed' },
+  login: { color: 'white', fontWeight: '600' }
+}
+
+/**
+ * This is a composite view-controller for the bar the shows up at the top of all the
+ * application views. Notice that it only listens for whether the user is authenicated
+ * or not.
+ */
 class Header extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isLoggedIn: false
+    }
+    this.onChange = this.onChange.bind(this);
+  }
+
+  /**
+   * Adds listeners to the relevant stores and loads the application configuration.
+   */
+  componentDidMount() {
+    LoginStore.addChangeListener(this.onChange);
+  }
+
+  componentWillUnmount() {
+    LoginStore.removeChangeListener(this.onChange);
+  }
+
+  onChange() {
+    if (LoginStore.isLoggedIn() != this.state.isLoggedIn) {
+      this.setState({ isLoggedIn: LoginStore.isLoggedIn() });
+    }
   }
 
   render() {
     var extraProps = {};
-    if (Login.getDisplayName()) extraProps.displayName = Login.getDisplayName();
-    if (Login.getEmail()) extraProps.email = Login.getEmail();
-    if (Login.getAuthProvider()) extraProps.authProvider = Login.getAuthProvider();
-    if (Login.getAvatarUrl()) extraProps.avatarUrl = Login.getAvatarUrl();    
+    if (LoginStore.getDisplayName()) extraProps.displayName = LoginStore.getDisplayName();
+    if (LoginStore.getEmail()) extraProps.email = LoginStore.getEmail();
+    if (LoginStore.getAuthProvider()) extraProps.authProvider = LoginStore.getAuthProvider();
+    if (LoginStore.getAvatarUrl()) extraProps.avatarUrl = LoginStore.getAvatarUrl();    
 
     var loginInfo = <LoginInfo onSignIn={LoginAction.onSignIn} 
                            onSignOut={LoginAction.onSignOut} 
-//                           isSignedIn={this.props.isSignedIn}
+                           isSignedIn={this.state.isLoggedIn}
                            style={style.login}
                            {...extraProps} />;
 
