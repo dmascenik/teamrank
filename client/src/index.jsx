8eb4239fae54159acf2e0bcfaf4c374c876b2d45
Dan Mascenik <dan@zoomdata.com>
2015-10-30 16:29:56 -0400

    Requires auth on enter, but doesn't kick you out on signout

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, IndexRoute, Route } from 'react-router'
-import createBrowserHistory from 'history/lib/createBrowserHistory'
+import { Router, IndexRoute, Route } from 'react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+
+import { LoginStore } from './flux/store';
 
 var TeamRankApp = require('./flux/component/TeamRankApp.jsx');
 var Splash = require('./flux/component/composite/Splash.jsx');
@@ -11,11 +13,17 @@ var injectTapEventPlugin = require('react-tap-event-plugin');
 injectTapEventPlugin();
 // ReactDOM.render(<TeamRankApp /> ,  document.getElementById("app"));
 
+function requireAuth(nextState, replaceState) {
+  if (!LoginStore.isLoggedIn()) {
+    replaceState({ nextPathname: nextState.location.pathname }, '/');
+  }
+}
+
 ReactDOM.render((
   <Router history={createBrowserHistory()}>
     <Route path="/" component={TeamRankApp}>
       <IndexRoute component={Splash}/>
-      <Route path="one" component={LoggedIn1}/>
+      <Route path="one" component={LoggedIn1} onEnter={requireAuth} />
     </Route>
   </Router>
 ), document.getElementById("app"));
